74065909c7ec1f8eef9fd1a42ea598781e0fd010
mikexufcb <mikexufcb@outlook.com>
2023-08-15 14:15:57 +0800

    8.15

--- myapp/models.py
+++ myapp/models.py
@@ -12,28 +12,47 @@ class Users(models.Model):
     dept = models.CharField(db_column='dept', max_length=50, blank=True, null=True)
 
 
+
+
+
 class Projects(models.Model):
-    projectid = models.AutoField(db_column='ProjectID', primary_key=True)  # Field name made lowercase.
-    project_name = models.CharField(db_column='Project_name', max_length=200)  # Field name made lowercase.
-    pm = models.ForeignKey('Users', models.DO_NOTHING, db_column='PM', blank=True, null=True)  # Field name made lowercase.
-    estimated_budget = models.IntegerField(db_column='Estimated_budget', blank=True, null=True)  # Field name made lowercase.
-    irr = models.IntegerField(db_column='IRR', blank=True, null=True)  # Field name made lowercase.
-    project_status = models.IntegerField(db_column='Project_status', blank=True, null=True)  # Field name made lowercase.
-    project_type = models.CharField(db_column='Project_type', max_length=30, blank=True, null=True)  # Field name made lowercase.
-    project_manager = models.CharField(db_column='Project_manager', max_length=50, blank=True, null=True)  # Field name made lowercase.
-    project_team = models.CharField(db_column='Project_team', max_length=200, blank=True, null=True)  # Field name made lowercase.
-    timing_kickoff = models.DateField(db_column='Timing_kickoff', blank=True, null=True)  # Field name made lowercase.
-    timing_closure = models.DateField(db_column='Timing_closure', blank=True, null=True)  # Field name made lowercase.
-    timing_milestone1 = models.DateField(db_column='Timing_milestone1', blank=True, null=True)  # Field name made lowercase.
-    timing_milestone2 = models.DateField(db_column='Timing_milestone2', blank=True, null=True)  # Field name made lowercase.
-    timing_milestone3 = models.DateField(db_column='Timing_milestone3', blank=True, null=True)  # Field name made lowercase.
-    timing_milestone4 = models.DateField(db_column='Timing_milestone4', blank=True, null=True)  # Field name made lowercase.
-    main_target = models.CharField(db_column='Main_target', max_length=2000, blank=True, null=True)  # Field name made lowercase.
-    boundary_conditions = models.CharField(db_column='Boundary_conditions', max_length=2000, blank=True, null=True)  # Field name made lowercase.
-    out_of_scope = models.CharField(db_column='Out_of_scope', max_length=2000, blank=True, null=True)  # Field name made lowercase.
-    steering_committee = models.CharField(db_column='Steering_committee', max_length=2000, blank=True, null=True)  # Field name made lowercase.
-    facilitator = models.CharField(db_column='Facilitator', max_length=2000, blank=True, null=True)  # Field name made lowercase.
-    risk_uncertainties = models.CharField(db_column='Risk_uncertainties', max_length=2000, blank=True, null=True)  # Field name made lowercase.
+    STATUS_CHOICES = [
+    ('red','red'),
+    ('yellow','yellow'),
+    ('green','green'),
+    ('grey','grey'),
+    ]
+
+    TYPE_CHOICES = {
+    ('SPA','SPA'),
+    ('PLP','PLP'),
+    }
+
+    projectid = models.AutoField(db_column='ProjectID', primary_key=True) # Field name made lowercase.
+    project_name = models.CharField(db_column='Project_name', max_length=200) # Field name made lowercase.
+    pm = models.ForeignKey('Users', models.DO_NOTHING, db_column='PM', blank=True, null=True) # Field name made lowercase.
+    estimated_budget = models.IntegerField(db_column='Estimated_budget', blank=True, null=True) # Field name made lowercase.
+    irr = models.IntegerField(db_column='IRR', blank=True, null=True) # Field name made lowercase.
+    project_status = models.CharField(db_column='Project_status',choices=STATUS_CHOICES, max_length=10, default='green') # Field name made lowercase.
+    project_type = models.CharField(db_column='Project_type',choices=TYPE_CHOICES, max_length=30, blank=True, null=True) # Field name made lowercase.
+    project_manager = models.CharField(db_column='Project_manager', max_length=50, blank=True, null=True) # Field name made lowercase.
+    project_team = models.CharField(db_column='Project_team', max_length=200, blank=True, null=True) # Field name made lowercase.
+    timing_kickoff = models.DateField(db_column='Timing_kickoff', blank=True, null=True) # Field name made lowercase.
+    timing_closure = models.DateField(db_column='Timing_closure', blank=True, null=True) # Field name made lowercase.
+    timing_milestone1 = models.DateField(db_column='Timing_milestone1', blank=True, null=True) # Field name made lowercase.
+    timing_milestone2 = models.DateField(db_column='Timing_milestone2', blank=True, null=True) # Field name made lowercase.
+    timing_milestone3 = models.DateField(db_column='Timing_milestone3', blank=True, null=True) # Field name made lowercase.
+    timing_milestone4 = models.DateField(db_column='Timing_milestone4', blank=True, null=True) # Field name made lowercase.
+    main_target = models.CharField(db_column='Main_target', max_length=2000, blank=True, null=True) # Field name made lowercase.
+    boundary_conditions = models.CharField(db_column='Boundary_conditions', max_length=2000, blank=True, null=True) # Field name made lowercase.
+    out_of_scope = models.CharField(db_column='Out_of_scope', max_length=2000, blank=True, null=True) # Field name made lowercase.
+    steering_committee = models.CharField(db_column='Steering_committee', max_length=2000, blank=True, null=True) # Field name made lowercase.
+    facilitator = models.CharField(db_column='Facilitator', max_length=2000, blank=True, null=True) # Field name made lowercase.
+    risk_uncertainties = models.CharField(db_column='Risk_uncertainties', max_length=2000, blank=True, null=True) # Field name made lowercase.
+
+
+
+
 
 class Test(models.Model):
     name = models.CharField(db_column='name', max_length=200)
--- myapp/views.py
+++ myapp/views.py
@@ -4,22 +4,19 @@ from django.shortcuts import render
 import requests
 import json
 from .models import Users, Projects, Announcement, KickOff, Milestones, Closure
-###################### Login Page 登录页面
+#from django.views.decorators.csrf import csrf_protect
 
+###################### Login Page 登录页面
 
 def start_page(request):
     if request.method == "POST":
         username = request.POST.get('username')
-        password = request.POST.get('password')
-
+        #password = request.POST.get('password')
         result = sign_in(username)
         #print(result.pm)
 
         if result:
             return redirect('navigation_page') # 跳转到导航页面
-
-        
-
         else:
             message = "Invalid user!" # 如果用户名或密码错误，添加一个消息
     else:
@@ -28,30 +25,40 @@ def start_page(request):
     context = {
         "message": message
     }
-    
     return render(request, 'login.html', context)
 
 
 
 ###################### Navigation Page 导航页面
 
-
 def navigation_page(request):
-    user_projects = get_user_projects('uig27066')
-    return render(request, 'navigation_page.html',{'projects':user_projects})
 
+    user_projects = get_user_projects('uig27066') # 从数据库中获取特定用户的项目
+    all_projects = Projects.objects.all() # 从数据库中获取所有项目
 
+    context = {# 将特定用户的项目和所有项目传递给模板以供显示
+        'user_projects': user_projects,
+        'all_projects': all_projects
+    }
+    return render(request, 'navigation_page.html', context)
 
 
 ###################### Start New Project 用户创建新项目网页
 
-from django.http import HttpResponse
 
-from django.shortcuts import render
 def start_new_project(request):
+    # 创建字段名称映射
+    date_attributes_mapping = {
+        "Kickoff Time": "timing_kickoff",
+        "Closure Time": "timing_closure",
+        "Milestone 1 Time": "timing_milestone1",
+        "Milestone 2 Time": "timing_milestone2",
+        "Milestone 3 Time": "timing_milestone3",
+        "Milestone 4 Time": "timing_milestone4"
+    }
+
     context = {
         "attributes_before_project_type": [
-            "Project ID",
             "Project Name",
             "Project Manager", 
             "Estimated Budget",
@@ -62,14 +69,9 @@ def start_new_project(request):
         "facilitators": ["Jing Yang", "Lisa Shi", "Runkun Wang"],
         "steering_committee": ["Jeff Wang", "Yuan Zhou", "Yi Sun", "Brian Yang", "Li Yan", "Tony Huang", "Sam Guo", "Rowena Tao","Covey Wang", "Hairui Liu", "Bingyan Wang", "Abhishek Kumar"],
         
-        "date_attributes": [
-            "Kickoff Time",
-            "Closure Time",
-            "Milestone 1 Time",
-            "Milestone 2 Time",
-            "Milestone 3 Time",
-            "Milestone 4 Time"
-        ],
+        # 使用映射中的模型字段名称
+        "date_attributes": list(date_attributes_mapping.values()),
+
         "attributes_after_project_type_and_dates": [
             "Main Target",
             "Boundary Conditions",
@@ -77,40 +79,32 @@ def start_new_project(request):
             "Steering committee",
             "Facilitator",
             "Risk and Uncertainties"
-        ]
+        ],
+        # 保留映射，以备在模板中显示友好的名称
+        "date_attributes_mapping": date_attributes_mapping
     }
-    
+
     return render(request, 'start_new_project.html', context)
 
 
 
-#########################See Existing Project Page用户阅读旧项目网页
 
-
-def project_site(request):
+def project_site(request):#########################See Existing Project Page用户阅读旧项目网页
     return render(request, 'project_site.html')
 
-######################### Announcement Review Page 
-
-def preview_announcement(request):
+def preview_announcement(request):######################### Announcement Review Page
     return render(request, 'preview_announcement.html', {'title': 'Announcement Review'})
 
-######################### Kick off Review Page 
-
-def kickoff_review(request):
+def kickoff_review(request):######################### Kick off Review Page 
     return render(request, 'preview_kickoff.html')
 
-######################### Milestone Review Page 
-
-def preview_milestone(request):
+def preview_milestone(request):######################### Milestone Review Page 
     return render(request, 'preview_milestone.html')
 
-######################### Closure Review Page 
-
-def preview_closure(request):
+def preview_closure(request):######################### Closure Review Page 
     return render(request, 'preview_closure.html')
 
-
+"""
 def sign_in(username):
 
     data = {}
@@ -130,6 +124,28 @@ def sign_in(username):
             return instance
     else:
         return False
+"""
+
+def sign_in(username):
+    try:
+        instance = Users.objects.get(pm=username)
+        return instance
+    except Users.DoesNotExist:
+        # 如果用户不存在，这里需要从其他数据源获取用户数据（这部分代码未给出）
+        # 假设从其他数据源获取的数据放在了data字典中
+        # 以下部分代码进行了略微调整
+        string = data.get('data', '{}')
+        data = json.loads(string)
+        attribute = data.get('attributes', {})
+        name = attribute.get('displayName', [''])[0]
+        email = attribute.get('mail', [''])[0]
+        department = attribute.get('department', [''])[0]
+        
+        info = {'pm': username, 'name': name, 'email': email, 'department': department}
+        instance = add_user(info)
+        return instance
+
+
 
 
 def add_user(info):
@@ -154,35 +170,157 @@ def get_user_projects(pm):
 def new_project(pm):
     pass
 
-
     #user_projects = {'project1':'project info', 'project2':'project info'}
     #return user_projects
 
-from django.shortcuts import render, redirect
-from .models import Projects
+
+from django.shortcuts import redirect
+from .models import Projects  # 假设你有一个名为Project的模型
+
+"""
 
 def submit_new_project(request):
     if request.method == 'POST':
-        # 从 POST 数据中获取表单数据
-        project_id = request.POST.get('project_id')
+        # 从POST请求中获取数据
         project_name = request.POST.get('project_name')
+        projectid = request.POST.get('project_id')  # 调整了变量名以匹配模型
         estimated_budget = request.POST.get('estimated_budget')
         irr = request.POST.get('irr')
         project_manager = request.POST.get('project_manager')
-        # ... 获取其他表单字段
+        project_type = request.POST.get('project_type')
+        
+        # 获取项目的开始和结束日期
+        timing_kickoff = request.POST.get('start-date')
+        timing_closure = request.POST.get('end-date')
 
-        # 创建一个新的项目实例并保存到数据库
+        # 从动态添加的字段中提取数据
+        main_target = ' '.join([request.POST.get(f'main-target_input_{i}', '') for i in range(1, 5)])
+        boundary_conditions = ' '.join([request.POST.get(f'boundary_input_{i}', '') for i in range(1, 5)])
+        out_of_scope = ' '.join([request.POST.get(f'out-of-scope_input_{i}', '') for i in range(1, 5)])
+        risk_uncertainties = ' '.join([request.POST.get(f'risk_input_{i}', '') for i in range(1, 5)])
+
+        # 保存项目数据到数据库
         project = Projects(
-            project_id=project_id,
             project_name=project_name,
+            projectid=projectid,
             estimated_budget=estimated_budget,
             irr=irr,
             project_manager=project_manager,
-            # ... 其他字段
+            project_type=project_type,
+            timing_kickoff=timing_kickoff,
+            timing_closure=timing_closure,
+            main_target=main_target,
+            boundary_conditions=boundary_conditions,
+            out_of_scope=out_of_scope,
+            risk_uncertainties=risk_uncertainties,
         )
         project.save()
 
-        # 提交完成后，重定向到 navigation 页面
         return redirect('navigation_page')
 
-    return render(request, 'start_new_project.html')
+    else:
+        return redirect('start_new_project_page')
+
+"""
+
+def submit_new_project(request):
+    if request.method == 'POST':
+        # 从POST请求中获取数据
+        project_name = request.POST.get('project_name')
+        projectid = request.POST.get('project_id')  # 调整了变量名以匹配模型
+        estimated_budget = request.POST.get('estimated_budget')
+        irr = request.POST.get('irr')
+        project_manager = request.POST.get('project_manager')
+        project_type = request.POST.get('project_type')
+        
+        # 获取项目的开始和结束日期
+        timing_kickoff = request.POST.get('timing_kickoff')
+        timing_closure = request.POST.get('timing_closure')
+        timing_milestone1 = request.POST.get('timing_milestone1')
+        timing_milestone2 = request.POST.get('timing_milestone2')
+        timing_milestone3 = request.POST.get('timing_milestone3')
+        timing_milestone4 = request.POST.get('timing_milestone4')
+
+
+        # 从动态添加的字段中提取数据
+        main_target = ' '.join([request.POST.get(f'main-target_input_{i}', '') for i in range(1, 5)])
+        boundary_conditions = ' '.join([request.POST.get(f'boundary-conditions_input_{i}', '') for i in range(1, 5)])
+        out_of_scope = ' '.join([request.POST.get(f'out-of-scope_input_{i}', '') for i in range(1, 5)])
+        risk_uncertainties = ' '.join([request.POST.get(f'risk-and-uncertainties_input_{i}', '') for i in range(1, 5)])
+
+    
+
+        # 保存项目数据到数据库
+        project = Projects(
+            project_name=project_name,
+            projectid=projectid,
+            estimated_budget=estimated_budget,
+            irr=irr,
+            project_manager=project_manager,
+            project_type=project_type,
+            timing_kickoff=timing_kickoff,
+            timing_closure=timing_closure,
+            main_target=main_target,
+            boundary_conditions=boundary_conditions,
+            out_of_scope=out_of_scope,
+            risk_uncertainties=risk_uncertainties,
+            timing_milestone1=timing_milestone1,
+            timing_milestone2=timing_milestone2,
+            timing_milestone3=timing_milestone3,
+            timing_milestone4=timing_milestone4,
+        )
+        project.save()
+
+        # 保存数据到session中，供check_submitted_data视图使用
+        request.session['submitted_data'] = {
+            'project_name': project_name,
+            'projectid': projectid,
+            'estimated_budget': estimated_budget,
+            'irr': irr,
+            'project_manager': project_manager,
+            'project_type': project_type,
+            'timing_kickoff': timing_kickoff,
+            'timing_closure': timing_closure,
+            'main_target': main_target,
+            'boundary_conditions': boundary_conditions,
+            'out_of_scope': out_of_scope,
+            'risk_uncertainties': risk_uncertainties,
+            'timing_milestone1': timing_milestone1,
+            'timing_milestone2': timing_milestone2,
+            'timing_milestone3': timing_milestone3,
+            'timing_milestone4': timing_milestone4,
+        }
+
+        # 重定向到check_submitted_data视图
+        return redirect('check_data')
+
+    else:
+        return redirect('start_new_project_page')
+
+
+
+#---------------
+
+"""
+from django.shortcuts import render
+
+def check_submitted_data(request):
+    if request.method == 'POST':
+        # 如果是POST请求，获取所有提交的数据
+        data = request.POST
+        # 将数据渲染到一个新的模板上
+        return render(request, 'check_data.html', {'data': data})
+    else:
+        # 如果不是POST请求，重定向到start_new_project页面
+        return redirect('start_new_project_page')
+'"""
+
+def check_submitted_data(request):
+    if 'submitted_data' in request.session:
+        # 从session中获取数据
+        data = request.session['submitted_data']
+        # 渲染数据到模板
+        return render(request, 'check_data.html', {'data': data})
+    else:
+        # 如果session中没有数据，重定向到start_new_project页面
+        return redirect('start_new_project_page')
